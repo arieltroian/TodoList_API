6c6d77e82239a44bcaead815db9d2b50c43f15e5
arieltroian <arieltroian@outlook.com>
2025-12-27 15:16:20 -0300

    feat: create and configure Todo entity

---
+++ todolist/src/main/java/com/arieltroian/todolist/entity/Todo.java
@@ -0,0 +1,56 @@
+package com.arieltroian.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todos")
+
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean finished;
+    private int priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
