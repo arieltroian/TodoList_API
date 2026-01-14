f5912a77a1667d14630ac9a187856747c72492bf
arieltroian <arieltroian@outlook.com>
2026-01-14 14:55:48 -0300

    feat: add UserRepository for database access

---
+++ src/main/java/com/arieltroian/todolist/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.arieltroian.todolist.repository;
+
+import com.arieltroian.todolist.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
